f14b2b259746182e33dab1d7d6e4c0eeb02c6698
Gaurav Singh <gaurav.singh55@gmail.com>
2017-11-15 00:05:48 +0530

    Python list creation operations big O complexity

---
+++ algorithmic_analysis/python_ds_efficiency/python_list.py
@@ -0,0 +1,46 @@
+from timeit import Timer, timeit
+# Different ways to create a python list of n numbers starting with 0
+
+
+def test1():
+    list_ = []
+    for i in range(1000):
+        list_ += [i]
+
+    return list_
+
+
+def test2():
+    list_ = []
+    for i in range(1000):
+        list_.append(i)
+
+    return list_
+
+
+def test3():
+    list_ = [i for i in range(1000)]
+
+    return list_
+
+
+def test4():
+    return list(range(1000))
+
+
+t1 = Timer('test1()', 'from __main__ import test1')
+print('concat performance ', t1.timeit(number=1000), ' ms')
+
+t2 = Timer('test2()', 'from __main__ import test2')
+print('append performance ', t2.timeit(number=1000), ' ms')
+
+t3 = Timer('test3()', 'from __main__ import test3')
+print('list comprehension performance ', t3.timeit(number=1000), ' ms')
+
+t4 = Timer('test4()', 'from __main__ import test4')
+print('list constructor wrapped over range performance ',
+      t4.timeit(number=1000), ' ms')
+
+# from __name__ import test1 means
+# import test1 from __name__ namespace into the namespace
+# which timeit setups up for the timing experiment.
