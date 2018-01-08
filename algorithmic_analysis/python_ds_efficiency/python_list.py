a7e1dfd2d27a34f755751eb24b36153a46b536bc
Gaurav Singh <gaurav.singh55@gmail.com>
2018-01-08 18:22:34 +0530

    Added no's infront of files to indicate sequence.

--- algorithmic_analysis/python_ds_efficiency/python_list.py
+++ algorithmic_analysis/python_ds_efficiency/python_list.py
@@ -28,18 +28,18 @@ def test4():
     return list(range(1000))
 
 
-# t1 = Timer('test1()', 'from __main__ import test1')
-# print('concat performance ', t1.timeit(number=1000), ' ms')
+t1 = Timer('test1()', 'from __main__ import test1')
+print('concat performance ', t1.timeit(number=1000), ' ms')
 
-# t2 = Timer('test2()', 'from __main__ import test2')
-# print('append performance ', t2.timeit(number=1000), ' ms')
+t2 = Timer('test2()', 'from __main__ import test2')
+print('append performance ', t2.timeit(number=1000), ' ms')
 
-# t3 = Timer('test3()', 'from __main__ import test3')
-# print('list comprehension performance ', t3.timeit(number=1000), ' ms')
+t3 = Timer('test3()', 'from __main__ import test3')
+print('list comprehension performance ', t3.timeit(number=1000), ' ms')
 
-# t4 = Timer('test4()', 'from __main__ import test4')
-# print('list constructor wrapped over range performance ',
-#       t4.timeit(number=1000), ' ms')
+t4 = Timer('test4()', 'from __main__ import test4')
+print('list constructor wrapped over range performance ',
+      t4.timeit(number=1000), ' ms')
 
 # from __name__ import test1 means
 # import test1 from __name__ namespace into the namespace
