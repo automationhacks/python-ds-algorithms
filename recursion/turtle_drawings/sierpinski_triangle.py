9783a39d7063c2836b65f365ba232985f8feb5cf
Gaurav Singh <gaurav.singh@go-jek.com>
2020-05-04 08:21:33 +0530

    Sierpinkski triangle (added books page)

--- recursion/turtle_drawings/sierpinski_triangle.py
+++ recursion/turtle_drawings/sierpinski_triangle.py
@@ -1,3 +1,5 @@
+# https://runestone.academy/runestone/books/published/pythonds/Recursion/pythondsSierpinskiTriangle.html
+
 import turtle
 from dataclasses import dataclass
 
@@ -26,30 +28,30 @@ def get_mid(point1, point2):
     return Point(x, y)
 
 
-def siepinski(points, degree, my_turtle):
+def sierpinski(points, degree, my_turtle):
     color_map = ['blue', 'red', 'green', 'white', 'yellow', 'violet', 'orange']
     draw_triangle(points, color_map[degree], my_turtle)
 
     if degree > 0:
         sub_triangle = [points[0], get_mid(points[0], points[1]),
                         get_mid(points[0], points[2])]
-        siepinski(sub_triangle, degree - 1, my_turtle)
+        sierpinski(sub_triangle, degree - 1, my_turtle)
 
         sub_triangle = [points[1], get_mid(points[0], points[1]),
                         get_mid(points[1], points[2])]
-        siepinski(sub_triangle, degree - 1, my_turtle)
+        sierpinski(sub_triangle, degree - 1, my_turtle)
 
         sub_triangle = [points[2], get_mid(points[2], points[1]),
                         get_mid(points[0], points[2])]
-        siepinski(sub_triangle, degree - 1, my_turtle)
+        sierpinski(sub_triangle, degree - 1, my_turtle)
 
 
 def main():
     my_turtle = turtle.Turtle()
     win = turtle.Screen()
     my_turtle.speed(10)
-    points = [Point(-100, -50), Point(0, 100), Point(100, -50)]
-    siepinski(points, 3, my_turtle)
+    points = [Point(-300, -150), Point(0, 300), Point(300, -150)]
+    sierpinski(points, 3, my_turtle)
     win.exitonclick()
 
 
