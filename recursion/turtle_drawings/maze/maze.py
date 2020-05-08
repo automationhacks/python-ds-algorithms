e2579939ac7405ef3b6a59e262210512e25130be
Gaurav Singh <gaurav.singh@go-jek.com>
2020-05-08 09:54:39 +0530

    Maze: Added complete program to search for path in maze

--- recursion/turtle_drawings/maze/maze.py
+++ recursion/turtle_drawings/maze/maze.py
@@ -30,6 +30,7 @@ class Maze:
         lower_left_y = -(self.rows_in_maze - 1) / 2 - 0.5
         upper_right_x = (self.columns_in_maze - 1) / 2 + 0.5
         upper_right_y = (self.rows_in_maze - 1) / 2 + 0.5
+        self.window.setup(width=600, height=600)
         self.window.setworldcoordinates(lower_left_x, lower_left_y,
                                         upper_right_x, upper_right_y)
 
@@ -53,11 +54,14 @@ class Maze:
     def draw_maze(self):
         self.turtle.speed(10)
         self.window.tracer(0)
-        for x, row in enumerate(self.maze_list):
-            for y, col in enumerate(row):
-                if col == OBSTACLE:
+
+        for y in range(self.rows_in_maze):
+            for x in range(self.columns_in_maze):
+                if self.maze_list[y][x] == OBSTACLE:
                     self.draw_centered_box(x + self.x_translate,
-                                           -y + self.y_translate, 'orange')
+                                           -y + self.y_translate,
+                                           'orange')
+
         self.turtle.color('black')
         self.turtle.fillcolor('blue')
         self.window.tracer()
@@ -77,10 +81,77 @@ class Maze:
             self.turtle.right(90)
         self.turtle.end_fill()
 
+    def move_turtle(self, x, y):
+        self.turtle.up()
+        self.turtle.setheading(self.turtle.towards(x + self.x_translate,
+                                                   -y + self.y_translate))
+        self.turtle.goto(x + self.x_translate, -y + self.y_translate)
+
+    def drop_breadcrumb(self, color):
+        self.turtle.dot(10, color)
+
+    def update_position(self, row, col, val=None):
+        if val:
+            self.maze_list[row][col] = val
+
+        self.move_turtle(col, row)
+        if val == PART_OF_PATH:
+            color = 'green'
+        elif val == OBSTACLE:
+            color = 'red'
+        elif val == 'TRIED':
+            color = 'black'
+        elif val == DEAD_END:
+            color = 'red'
+        else:
+            color = None
+
+        if color:
+            self.drop_breadcrumb(color)
+
+    def is_exit(self, row, col):
+        return (
+                row == 0 or
+                row == self.rows_in_maze - 1 or
+                col == 0 or
+                col == self.columns_in_maze - 1)
+
+    def __getitem__(self, idx):
+        return self.maze_list[idx]
+
+
+def search_from(maze, start_row, start_column):
+    maze.update_position(start_row, start_column)
+
+    pos = maze[start_row][start_column]
+
+    # Return false for all base cases
+    if pos in (OBSTACLE, TRIED, DEAD_END):
+        return False
+
+    if maze.is_exit(start_row, start_column):
+        maze.update_position(start_row, start_column, PART_OF_PATH)
+        return True
+    maze.update_position(start_row, start_column, TRIED)
+
+    # Use logical short circuiting to search in each direction
+    found = search_from(maze, start_row - 1, start_column) or \
+            search_from(maze, start_row + 1, start_column) or \
+            search_from(maze, start_row, start_column - 1) or \
+            search_from(maze, start_row, start_column + 1)
+
+    if found:
+        maze.update_position(start_row, start_column, PART_OF_PATH)
+    else:
+        maze.update_position(start_row, start_column, DEAD_END)
+    return found
+
 
 def main():
     my_maze = Maze('maze_input.txt')
     my_maze.draw_maze()
+    my_maze.update_position(my_maze.start_row, my_maze.start_col)
+    search_from(my_maze, my_maze.start_row, my_maze.start_col)
     my_maze.destroy_world()
 
 
