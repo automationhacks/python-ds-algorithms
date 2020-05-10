ea4a35945b95e46c33eca4e2442e20ab59efb5b3
Gaurav Singh <gaurav.singh@go-jek.com>
2020-05-10 11:42:04 +0530

    Merge sort implementation

---
+++ sorting/merge_sort.py
@@ -0,0 +1,46 @@
+def merge_sort(alist):
+    print(f'Splitting {alist}')
+
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        # slice operator is O(k), this can be avoided by passing start, end
+        # indexes into merge sort
+        left = alist[:mid]
+        right = alist[mid:]
+
+        # Assume list is sorted
+        merge_sort(left)
+        merge_sort(right)
+
+        i = 0
+        j = 0
+        k = 0
+
+        # Merge process
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                alist[k] = left[i]
+                i += 1
+            else:
+                alist[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            alist[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            alist[k] = right[j]
+            j += 1
+            k += 1
+
+    print(f'Merging {alist}')
+    return alist
+
+
+def test_merge_sort():
+    before = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    after = [17, 20, 26, 31, 44, 54, 55, 77, 93]
+    assert merge_sort(before) == after
