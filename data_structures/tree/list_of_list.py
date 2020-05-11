a3c98a82a0ed1f70bd2a0ac9997b2a0c351f2d06
Gaurav Singh <gaurav.singh@go-jek.com>
2020-05-11 21:40:27 +0530

    Added test to verify creation of tree using list of lists representation

--- data_structures/tree/list_of_list.py
+++ data_structures/tree/list_of_list.py
@@ -56,3 +56,20 @@ def get_left_child(root):
 
 def get_right_child(root):
     return root[2]
+
+
+def test_binary_tree_creation_using_classes():
+    root = binary_tree(3)
+    insert_left(root, 4)
+    insert_left(root, 5)
+    insert_right(root, 6)
+    insert_right(root, 7)
+
+    left = get_left_child(root)
+    assert left[0] == 5
+    set_root(left, 9)
+    assert left[0] == 9
+
+    insert_left(left, 11)
+    assert root == [3, [9, [11, [4, [], []], []], []], [7, [], [6, [], []]]]
+    assert get_right_child(get_right_child(root)) == [6, [], []]
